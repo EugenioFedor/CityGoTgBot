129bbef0f37129a0d3e4e7c79cda2eeac5c59aae
Evgeny <bntk96lg@mail.ru>
2025-12-08 20:05:47 +0500

    Четвертая итерация. Дополнил gitignore. Осталось добавить в файлы resources данные для работы с таблицами и токены.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
