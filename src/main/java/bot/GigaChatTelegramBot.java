c8f69a2fc53ce59b02c1705165f64c8f518ff3d6
Evgeny <bntk96lg@mail.ru>
2025-12-14 21:44:46 +0500

    5 итерация. Демо

--- src/main/java/bot/GigaChatTelegramBot.java
+++ src/main/java/bot/GigaChatTelegramBot.java
@@ -5,7 +5,6 @@ import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.citygo.config.BotConfig;
 import ru.citygo.sheets.GoogleSheetsHelper;
 
--- src/main/java/webhook/WebhookController.java
+++ src/main/java/webhook/WebhookController.java
@@ -1,6 +1,9 @@
 package ru.citygo.webhook;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.citygo.bot.GigaChatTelegramBot;
 
